70bfdfacbdb2785a665eff9230e21a63d4640fa2
Diego García <a20172367@pucp.edu.pe>
2021-07-20 13:06:50 -0500

    Metodo para loguearse

--- .idea/index.js
+++ .idea/index.js
@@ -39,8 +39,46 @@ app.get('/login', function (req, res) {
     res.sendFile(__dirname + '/public/login.html');
 });
 
+app.post('/form', function (request, response) {
+    usuario = request.body.user;
+    console.log(usuario);
+    var contra = request.body.pass;
+
+    console.log(contra);
+    var hash_sha256 = crypto.createHash("sha256");
+    hash_sha256.update(contra);
+    var sha256c = hash_sha256.digest("hex"); // mostrado como hexadecimal
+    console.log(sha256c);
+    var params = [usuario, sha256c]
+    if (usuario && contra) {
+        var sql = "select * from usuario where user =? and pass =?";
+        conn.query(sql, params, function (error, results) {
+            if (error || results == "") {
+                response.sendFile(__dirname + '/public/login.html');
+            } else {
+                booleano=true;
+                response.sendFile(__dirname + '/public/');
+            }
+        });
+    } else {
+        response.sendFile(__dirname + '/public/login.html');
+        response.end();
+    }
+});
+
 server.listen(3000, function () {
     console.log("Servidor corriendo en el puerto 3000");
 
 });
+if (booleano){
+    io.on('connection', function (socket) {
+        console.log("usuario conectado");
+        io.emit("usuarioIngresado", usuario);
+        socket.on("mensaje de chat", function (mensaje) {
+            console.log("mensaje del cliente " + usuario + ": " + mensaje);
+            io.emit("mensaje broadcast", usuario + ": " + mensaje);
+        });
+    });
+    booleano=false;
+}
 
